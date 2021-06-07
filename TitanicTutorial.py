b95db939cc05ba72d62ad936d683309b65fadac9
trevorbrewer <85421721+trevorbrewer@users.noreply.github.com>
2021-06-07 13:51:41 -0400

    Added random forest model

--- TitanicTutorial.py
+++ TitanicTutorial.py
@@ -8,3 +8,25 @@ import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
+
+train_data = pd.read_csv("/kaggle/input/titanic/train.csv")
+train_data.head()
+
+test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
+test_data.head()
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")

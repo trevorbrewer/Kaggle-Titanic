cd94c9fa9882abef40727e5b45803e6b28b56d28
trevorbrewer <85421721+trevorbrewer@users.noreply.github.com>
2021-06-07 13:55:44 -0400

    added comments

--- TitanicTutorial.py
+++ TitanicTutorial.py
@@ -4,29 +4,36 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
+#print out each file name in the given directory
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
+#use pandas to read the training and test data
 train_data = pd.read_csv("/kaggle/input/titanic/train.csv")
 train_data.head()
 
 test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
 test_data.head()
 
+#create random forest based on class, sex. siblings, and parch
 from sklearn.ensemble import RandomForestClassifier
 
+#y equals all instances in training data where the person survived
 y = train_data["Survived"]
 
+#convert categorical variables into dummy variables (1 or 0)
 features = ["Pclass", "Sex", "SibSp", "Parch"]
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
+#fit model
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X, y)
 predictions = model.predict(X_test)
 
+#save output
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
 output.to_csv('my_submission.csv', index=False)
 print("Your submission was successfully saved!")
